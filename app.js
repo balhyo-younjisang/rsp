c670a212f128f48bb36a297332dbe21ad0f3543b
balhyo-younjisang <yjs12180825@gmail.com>
2022-09-10 14:03:47 +0900

    완성

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
 var rockBtn = document.getElementById("rk");
 var scissorsBtn = document.getElementById("sc");
 var paperBtn = document.getElementById("pp");
-var show = document.getElementById("player");
+var choice = document.getElementById("player-choice");
+var computerChoice = document.getElementById("computer-choice");
+var options = ["rock", "paper", "scissors"];
+var result = document.getElementById("result-container");
+function ComputerChoiceHandler() {
+    var computer = Math.floor(Math.random() * options.length);
+    //console.log(options[computer]);
+    return options[computer];
+}
+function whoWin(player, computer) {
+    if (player === computer) {
+        console.log("draw");
+        result.innerText = "draw";
+    }
+    else if (player === "rock") {
+        if (computer === "scissors")
+            result.innerText = "you Win!";
+        else
+            result.innerText = "you lose";
+    }
+    else if (player === "paper") {
+        if (computer === "scissors")
+            result.innerText = "you lose!";
+        else
+            result.innerText = "you win!";
+    }
+    else if (player === "scissors") {
+        if (computer === "paper")
+            result.innerText = "you lose!";
+        else
+            result.innerText = "you win!";
+    }
+}
 function onClickRock() {
-    show.innerHTML = "✊";
+    //console.log("rock");
+    var choiceTwo = ComputerChoiceHandler();
+    choice.src = "imgs/rock.png";
+    computerChoice.src = "imgs/".concat(choiceTwo, ".png");
+    whoWin("rock", choiceTwo);
 }
 function onClickScs() {
-    show.innerHTML = "✌️";
+    //console.log("scissors");
+    var choiceTwo = ComputerChoiceHandler();
+    choice.src = "imgs/scissors.png";
+    computerChoice.src = "imgs/".concat(choiceTwo, ".png");
+    whoWin("scissors", choiceTwo);
 }
 function onClickPaper() {
-    show.innerHTML = "🖐️";
+    //console.log("paper");
+    var choiceTwo = ComputerChoiceHandler();
+    choice.src = "imgs/paper.png";
+    computerChoice.src = "imgs/".concat(choiceTwo, ".png");
+    whoWin("paper", choiceTwo);
 }
-rockBtn === null || rockBtn === void 0 ? void 0 : rockBtn.addEventListener("click", onClickRock);
-scissorsBtn === null || scissorsBtn === void 0 ? void 0 : scissorsBtn.addEventListener("click", onClickScs);
-paperBtn === null || paperBtn === void 0 ? void 0 : paperBtn.addEventListener("click", onClickPaper);
+rockBtn.addEventListener("click", onClickRock);
+scissorsBtn.addEventListener("click", onClickScs);
+paperBtn.addEventListener("click", onClickPaper);
--- app.ts
+++ app.ts
@@ -1,12 +1,58 @@
-const rockBtn = document.getElementById("rk") as Element;
-const scissorsBtn = document.getElementById("sc") as Element;
-const paperBtn = document.getElementById("pp") as Element;
+const rockBtn = document.getElementById("rk") as HTMLButtonElement;
+const scissorsBtn = document.getElementById("sc") as HTMLButtonElement;
+const paperBtn = document.getElementById("pp") as HTMLButtonElement;
+const choice = document.getElementById("player-choice") as HTMLImageElement;
+const computerChoice = document.getElementById(
+  "computer-choice"
+) as HTMLImageElement;
+const options = ["rock", "paper", "scissors"];
+const result = document.getElementById("result-container") as HTMLElement;
 
-function onClickRock(): void {}
+function ComputerChoiceHandler(): string {
+  let computer = Math.floor(Math.random() * options.length);
+  //console.log(options[computer]);
+  return options[computer];
+}
 
-function onClickScs(): void {}
+function whoWin(player: string, computer: string): void {
+  if (player === computer) {
+    console.log("draw");
+    result.innerText = "draw";
+  } else if (player === "rock") {
+    if (computer === "scissors") result.innerText = "you Win!";
+    else result.innerText = "you lose";
+  } else if (player === "paper") {
+    if (computer === "scissors") result.innerText = "you lose!";
+    else result.innerText = "you win!";
+  } else if (player === "scissors") {
+    if (computer === "paper") result.innerText = "you win!";
+    else result.innerText = "you lose";
+  }
+}
 
-function onClickPaper(): void {}
+function onClickRock(): void {
+  //console.log("rock");
+  const choiceTwo = ComputerChoiceHandler();
+  choice.src = "imgs/rock.png";
+  computerChoice.src = `imgs/${choiceTwo}.png`;
+  whoWin("rock", choiceTwo);
+}
+
+function onClickScs(): void {
+  //console.log("scissors");
+  const choiceTwo = ComputerChoiceHandler();
+  choice.src = "imgs/scissors.png";
+  computerChoice.src = `imgs/${choiceTwo}.png`;
+  whoWin("scissors", choiceTwo);
+}
+
+function onClickPaper(): void {
+  //console.log("paper");
+  const choiceTwo = ComputerChoiceHandler();
+  choice.src = "imgs/paper.png";
+  computerChoice.src = `imgs/${choiceTwo}.png`;
+  whoWin("paper", choiceTwo);
+}
 
 rockBtn.addEventListener("click", onClickRock);
 scissorsBtn.addEventListener("click", onClickScs);
